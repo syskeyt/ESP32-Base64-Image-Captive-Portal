516ad700f56cd3dac47bd99c1d889f8e59d23ba4
syskeyt <103950926+syskeyt@users.noreply.github.com>
2022-08-18 20:50:46 -0400

    Delete HTMLPAGE.h

--- HTMLPAGE.h
+++
@@ -1,3 +0,0 @@
-const char HTMLPAGE[] PROGMEM = R"=====(
-<div><img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="image" /> </div>
-)=====";//file type here  ^^^                                                        // base64 code Here ^^^                                          //name here ^^^
